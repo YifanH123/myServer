0651efd546a421bc541947b07c8268a03e9462f3
Yifan Hu <yifanhu@Xiaocuis-iMac.local>
2020-10-16 17:43:32 -0700

    First Commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.get('/random', (req, res) => {
+  res.send('smth else');
+})
+
+app.get('/number1', (req, res) => {
+  res.send('1');
+})
+
+app.get('/number2', (req, res) => {
+  res.send('2');
+})
+
+app.get('/number3', (req, res) => {
+  res.send('3');
+})
+
+app.get('/number4', (req, res) => {
+  res.send('<button style="position:absolute; left: 50%; top: 50%">hi</button>');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+})
