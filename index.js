f1f54fd91726e96ede553b8d279ee7894fd95389
Yifan Hu <yifanhu@Xiaocuis-iMac.local>
2020-10-21 20:43:03 -0700

    reformated everything

--- index.js
+++ index.js
@@ -3,29 +3,29 @@ const app = express();
 var port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + "/calculator.html");
+	res.sendFile(__dirname + "/welcome.html");
 })
 
-app.get('/random', (req, res) => {
-  res.send('something else');
+app.get('/calc', (req, res) => {
+	res.sendFile(__dirname + "/calculator.html");
 })
 
-app.get('/number1', (req, res) => {
-  res.send('1');
+app.get('/desmos', (req, res) => {
+	res.send(`<button onclick="location.href='http://www.desmos.com'" type="button">3</button>`);
 })
 
 app.get('/number2', (req, res) => {
-  res.send('2');
+	res.send('2');
 })
 
 app.get('/number3', (req, res) => {
-  res.send(`<button onclick="location.href='http://www.desmos.com'"; type="button">3</button>`);
+	res.send(`<button onclick="location.href='http://www.desmos.com'"; type="button">3</button>`);
 })
 
 app.get('/button', (req, res) => {
-  res.send('<button style="position:absolute; left: 50%; top: 50%">hi</button>');
+	res.send('<button style="position:absolute; left: 50%; top: 50%">hi</button>');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+	console.log(`Example app listening at http://localhost:${port}`);
 })
